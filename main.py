aa105a545a9420197d504daa62228f884acee821
TobiasTR <tobiast98.kontakt@protonmail.com>
2022-01-07 00:27:07 +0100

    fixed bug where loop did not end ;)

--- main.py
+++ main.py
@@ -138,11 +138,13 @@ def driver_login(driver,user_data:LoginData):
     driver.find_element(By.ID, "ELEMENT_login_username").send_keys(user_data.username)
     driver.find_element(By.ID, "ELEMENT_login_password").send_keys(user_data.password)
     driver.find_element(By.ID, "ELEMENT_login_button").click()
-    while title != title_new:
-        time.sleep(3)
-        title_new = driver.title
+    time.sleep(args.delay)
 
     return driver
+
+"""while title != title_new:
+        time.sleep(3)
+        title_new = driver.title"""
 
 
 def get_ha_status(driver):
